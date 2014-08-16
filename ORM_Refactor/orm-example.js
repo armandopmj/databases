d42a919d1744ced953314900285c0c95dba5f9d7
armandopmj <armandopmj@gmail.com>
2014-08-15 22:32:37 -0700

    not done w sequelize

--- ORM_Refactor/orm-example.js
+++ ORM_Refactor/orm-example.js
@@ -4,7 +4,7 @@
  */
 
 var Sequelize = require("sequelize");
-var sequelize = new Sequelize("chatter", "root", "");
+var sequelize = new Sequelize("chattertest", "root", "");
 /* TODO this constructor takes the database name, username, then password.
  * Modify the arguments if you need to */
 
@@ -14,7 +14,7 @@ var User = sequelize.define('User', {
   username: Sequelize.STRING
 });
 
-var Message = sequelize.define('Message' {
+var Message = sequelize.define('Message', {
   userid: Sequelize.INTEGER,
   text: Sequelize.STRING,
   roomname: Sequelize.STRING
@@ -26,13 +26,14 @@ User.sync().success(function() {
   /* This callback function is called once sync succeeds. */
 
   // now instantiate an object and save it:
-  var newUser = User.build({username: "Jean Valjean"});
+  User.build({username: "Javert"}).save();
+  var newUser = User.build({username: "Luby Alverase"});
   newUser.save().success(function() {
 
     /* This callback function is called once saving succeeds. */
 
     // Retrieve objects from the database:
-    User.findAll({ where: {username: "Jean Valjean"} }).success(function(usrs) {
+    User.findAll().success(function(usrs) {
       // This function is called back with an array of matches.
       for (var i = 0; i < usrs.length; i++) {
         console.log(usrs[i].username + " exists");
